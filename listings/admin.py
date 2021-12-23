87d13d18e5cc001297318a09c4462cde95eaeb02
Ujah, Emmanuel <ujahemmanuel72@gmail.com>
2021-12-23 11:12:30 +0100

    customized admin display data

--- listings/admin.py
+++ listings/admin.py
@@ -3,5 +3,13 @@ from listings.models import Listing
 
 
 
+class ListingAdmin(admin.ModelAdmin):
+  list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+  list_display_links = ('id', 'title')
+  list_filter = ('realtor',)
+  list_editable = ('is_published',)
+  search_fields = ('title', 'description', 'price', 'city', 'zipcode', 'state', 'address')
+  list_per_page = 25
+
 # Register your models here.
-admin.site.register(Listing)
+admin.site.register(Listing, ListingAdmin)
